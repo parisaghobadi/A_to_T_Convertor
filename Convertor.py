79592f66cf17826e14bda93a5cd8509667339a81
Parisa Ghobadi <140270088+parisaghobadi@users.noreply.github.com>
2023-08-25 20:38:08 +0330

    Update Convertor.py

--- Convertor.py
+++ Convertor.py
@@ -1,63 +1,55 @@
 from tkinter import *
 import speech_recognition as sr
-from translate import Translator
+import translate as tr
 
+# for showing correct the translated texts
 global counter 
 counter = 2
 
-# MAIN 
-
-root=Tk()
+# main app
+root = Tk()
 root.title('Convertor')
 root.minsize(400,800)
 root.maxsize(400,800)
 
-
-# WIDGET & CONFIG 
-
+# widget & config
 def widget():
+    lbl1 = Label(root, text='Speak:')
+    lbl1.config(font=('Titr',22,'bold') , bg='yellow' , width=22)
+    lbl1.grid(row=0)
 
-    lb1=Label(root, text='Speak:')
-    lb1.config(font=('Titr',22,'bold'), bg='lime', width=22)
-    lb1.grid(row=0)
-
-    btn_start=Button(root, text='Start Convert' , command=get_voice)
-    btn_start.config(font=('Calibri',17,'bold'),bg='blue',fg='white', width=10)
-    btn_start.grid(row=1, pady=44)
-
+    btn_start = Button(root , text='Start Convert' , command=get_voice)
+    btn_start.config(font=('calibri',17 , 'bold') , bg='navy blue' , fg='white')
+    btn_start.grid(row=1 , pady=44)
 
 
-#GET VOICE & RECOGNITION
-
-
+# create an instance from modules
 r = sr.Recognizer()
+t = tr.Translator(to_lang='fa')
 
+# function for getting voice & recognition
 def get_voice():
     try:
-        with sr.Microphone() as src:           # ABBREVATION
+        with sr.Microphone() as src:
             audio = r.listen(src)
             text = r.recognize_google(audio)
-            translator = Translator(to_lang='fa')
-            text = translator.translate(text)
-            show_text(text)
+            translated_text = t.translate(text)
+            show_text(translated_text)
     except Exception as e:
-        print('error:',e)
-        
+        print('error :' , e)
 
-# FUCTION FOR SHOWING THE TRANSLATED TEXT
-
-def show_text(text):
+# function for showing the translated text
+def show_text(translated_text):
     global counter
-    lbl_name= f"lbl_{counter}"
-    lbl_name = Label(text = text)
-    lbl_name.config(font=('Calibri',20))
-    lbl_name.grid(row = counter)
-    counter +=1
+    lbl_name = f'lbl_{counter}'
+    lbl_name = Label(text= translated_text)
+    lbl_name.config(font= ('calibri',20))
+    lbl_name.grid(row= counter)
+    counter+=1 
 
 
-
-# CALL THE WIDGET FUNCTION 
-
+# call the widget function for showing the app
 widget()
+
 
 root.mainloop()
